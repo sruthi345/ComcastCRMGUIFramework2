ae74185d602ba6047840d8361389601d649939e2
Dell Inspiron15 <Dell Inspiron15@DESKTOP-VBUECEK>
2024-06-18 10:00:46 +0530

    product added

--- ComcastCRMGUIFramework2/src/main/java/com/comcast/crm/objectrepositoryUtility/ProductsPage.java
+++ ComcastCRMGUIFramework2/src/main/java/com/comcast/crm/objectrepositoryUtility/ProductsPage.java
@@ -1,5 +1,15 @@
 package com.comcast.crm.objectrepositoryUtility;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class ProductsPage {
+	@FindBy(name="searchlink")
+	private WebElement searchlink;
+	
+	public WebElement getsearchlink()
+	{
+		return  searchlink;
+	}
 
 }
